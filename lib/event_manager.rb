7ef46ee5529251dcf874fe7d61dfd53d4f80f74e
TriMadon <mehammed6789@gmail.com>
2023-09-12 15:18:24 +0300

    Get list of representatives for each attendee

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    )
+    legislators.officials.map(&:name).join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
 end
 
 puts 'Event Manager Initialized!'
@@ -17,6 +36,7 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode row[:zipcode]
+  legislators = legislators_by_zipcode zipcode
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
